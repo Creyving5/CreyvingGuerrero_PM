21531ddfef89809f0b1d12667c2418e1dea691df
Creyving5 <creyvingg240@gmail.com>
2024-10-02 23:25:39 -0400

    Cap 4
    new

---
+++ Cap4/Programa 4.1/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Cubo -1.
+El programa calcula el cubo de los 10 primeros numeros naturales con la ayuda de una funcion. en la solucion del problema se utiliza una variable global, aunque esto, como mas adelante, no es muy recomendable. */
+
+int cubo(void); /* Prototipo de función. */
+int I; /* Variable global. */
+
+void main(void)
+{
+int CUB;
+for (I = 1; I <= 10; I++)
+{
+CUB = cubo(); /* Llamada a la función cubo. */
+
+printf("\nEl cubo de %d es: %d", I, CUB);
+}
+}
+int cubo(void) /* Declaración de la función. */
+/* La función calcula el cubo de la variable global I. */
+{
+return (I*I*I);
+}
